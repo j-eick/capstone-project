25bad43aa93398ec4749a47154cf970450e42e6a
Joschka Eickhoff <jeickhoff@posteo.de>
2022-09-26 00:39:41 +0200

    outsourced header styles

---
+++ src/components/Header/StyledTitleContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledTitleContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	background-color: #a0c0d6;
+`;
+
+export default StyledTitleContainer;
