4a71665b8b0ea9fc95ef167c5177dffb49181a3d
Joschka Eickhoff <jeickhoff@posteo.de>
2022-09-29 00:22:55 +0200

    button click now adds a new dropdown component

--- src/components/CreateWorkout/form.js
+++ src/components/CreateWorkout/form.js
@@ -1,5 +1,7 @@
 // import {useStore} from 'zustand';
 
+import {useState} from 'react';
+
 import ButtonPlusWithBorder from '../buttons/plus';
 
 import {StyledForm, StyledP} from './styles';
@@ -7,28 +9,38 @@ import {StyledForm, StyledP} from './styles';
 console.clear();
 
 export default function CreateWorkoutForm() {
-	function clickHandler(event) {
-		event.preventDefault();
-		console.log(event.target);
-	}
+	// const [newCategory, setNewCategory] = useState(false);
+	const [dropdownList, setDropdownList] = useState([]);
 
+	const Dropdown = () => {
+		return (
+			<select name="category" id="categories">
+				<option value="choose...">choose</option>
+				<option value="warmup">Warm-Up</option>
+				<option value="core">Core</option>
+				<option value="legs">Legs</option>
+				<option value="torso">Torso</option>
+				<option value="shoulders">Shoulders</option>
+				<option value="hit">HIT</option>
+			</select>
+		);
+	};
 	return (
 		<>
 			<StyledP>Choose a workout type</StyledP>
 			<StyledForm>
 				<label>Pick a category</label>
-				<select name="category" id="categories">
-					<option value="choose...">choose</option>
-					<option value="warmup">Warm-Up</option>
-					<option value="core">Core</option>
-					<option value="legs">Legs</option>
-					<option value="torso">Torso</option>
-					<option value="shoulders">Shoulders</option>
-					<option value="hit">HIT</option>
-				</select>
-				<ButtonPlusWithBorder type="button" onClick={clickHandler}>
+				<Dropdown />
+				<ButtonPlusWithBorder
+					type="button"
+					onClick={event => {
+						event.preventDefault();
+						setDropdownList(dropdownList.concat(<Dropdown key={dropdownList} />));
+					}}
+				>
 					Add more
 				</ButtonPlusWithBorder>
+				{dropdownList}
 			</StyledForm>
 		</>
 	);
