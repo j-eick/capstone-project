4cf31cbff68e4f4f85b4133f988c003f823d6947
Joschka Eickhoff <jeickhoff@posteo.de>
2022-10-04 23:38:42 +0200

    minor changes

---
+++ src/components/buttons/BlockContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+	position: fixed;
+	bottom: 100px;
+	width: 100%;
+	text-align: center;
+`;
+
+export default StyledContainer;
