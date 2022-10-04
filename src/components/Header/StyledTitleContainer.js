a2830adf660e9fccb4e8e532845a20fb69d61929
Joschka Eickhoff <jeickhoff@posteo.de>
2022-10-04 23:31:01 +0200

    deprecated: file deleted

--- src/components/Header/StyledTitleContainer.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const StyledTitleContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	background-color: #a0c0d6;
-`;
-
-export default StyledTitleContainer;
